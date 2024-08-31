a4e6a9f36d3ca53da3a9a2d15b4c16d1c4ef3270
MrShekh <shekhasifofficial2022@gmail.com>
2024-08-31 11:24:43 +0530

    Initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "e28ed0f345db26a775fee5fb54697f7e",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
